9222c7b009595bf6f1283acfb62cf5d3b80a3b32
DESKTOP-VDDUFBH\admin <s3a432058@students.ncut.edu.tw>
2017-11-27 20:28:55 +0800

    將錯誤訊息區塊做成樣板

--- resources/views/admin/layouts/partials/validation.blade.php
+++ resources/views/admin/layouts/partials/validation.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/11/27
- * Time: 下午 08:26
- */
+@if($errors->any())
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+                @foreach( $errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+@endif
