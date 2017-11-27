b4666308c3a2f6ec39ab2168af18ba8815916e3b
DESKTOP-VDDUFBH\admin <s3a432058@students.ncut.edu.tw>
2017-11-27 19:34:46 +0800

    新增 Route(可以新增資料)

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('posts/{id}', ['as' => 'posts.show' , 'uses' => 'PostsController@show
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
 
+    Route::post('posts',['as' => 'admin.posts.store', 'uses' => 'AdminPostsController@store']);
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+
 });
