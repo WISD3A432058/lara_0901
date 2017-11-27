4bce3f7e711cd5cb111455176598c2c4511eea91
DESKTOP-VDDUFBH\admin <s3a432058@students.ncut.edu.tw>
2017-11-27 19:36:35 +0800

    更新資料(在 PostsController的 edit內取得舊資料)

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -31,8 +31,8 @@ class AdminPostsController extends Controller
 
     public function edit($id)
     {
-        $data = ['id' => $id];
-
-        return view('admin.posts.edit', $data);
+        $post=Post::find($id);
+        $data=['post'=>$post];
+        return view('admin.posts.edit',$data);
     }
 }
