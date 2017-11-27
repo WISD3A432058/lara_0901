059deaf661cd43ade674a4514f209fd46fabc777
DESKTOP-VDDUFBH\admin <s3a432058@students.ncut.edu.tw>
2017-11-27 20:00:16 +0800

    在 AdminPostsController的 destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -42,4 +42,11 @@ class AdminPostsController extends Controller
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
     }
+
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
+
 }
